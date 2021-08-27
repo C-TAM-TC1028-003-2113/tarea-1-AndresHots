5eceb108e52d6c16cbb69704cb00098028ec986e
AndresHots <89654918+AndresHots@users.noreply.github.com>
2021-08-27 12:52:22 -0500

    Update exercise.py

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+   # escribe tu código abajo de esta línea
+    nuevo = int(input("Ingrese la cantidad de juegos nuevos a adquirir: "))
+    usado = int(input("Ingrese la cantidad de juegos usados a adquirir: "))
+
+    total = (1000 * nuevo) + (350 * usado)
+
+    print("El monto total a pagar es de: $", total )
 
 
 if __name__ == '__main__':
