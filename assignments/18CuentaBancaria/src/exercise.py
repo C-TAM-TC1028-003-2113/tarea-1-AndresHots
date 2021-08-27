cbac9425f6cc83a233fd8f0dd2a141705c11006d
AndresHots <89654918+AndresHots@users.noreply.github.com>
2021-08-27 13:36:32 -0500

    Update exercise.py

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,6 +1,14 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+     x1 = float(input("Ingrese el salgo del mes antrior: "))
+    ing = float(input("Ingresos: "))
+    egr = float(input("egresos: "))
+    numCheq = int(input("Ingrese el numero de cheques expedidos: "))
+    interes= .75
+
+    total = ( x1 + ing + egr + (numCheq*13) ) * interes
+
+    print("El saldo final de la cuenta es: $", total)
 
 
 if __name__ == '__main__':
