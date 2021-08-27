bd3da69989d515d3037f543b3f0119079d93c001
AndresHots <89654918+AndresHots@users.noreply.github.com>
2021-08-27 12:04:36 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
-    # escribe tu código abajo de esta línea
-    pass
+
+    x1 = float(input("Dame x1: "))
+    y1 = float(input("Dame y1: "))
+    x2 = float(input("Dame x2: "))
+    y2 = float(input("Dame y2: "))
+    p = (y2 - y1) / (x2 - x1)
+    print("Pendiente:", p)
 
 
 if __name__ == '__main__':
