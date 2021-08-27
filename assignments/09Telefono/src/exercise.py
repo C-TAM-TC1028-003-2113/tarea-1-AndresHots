3fe4b90bafb52eac4ae7fe347e935ac77f18a752
AndresHots <89654918+AndresHots@users.noreply.github.com>
2021-08-27 12:33:28 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,7 +1,12 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
-
+     sms = int(input("Ingrese el numero de mensajes enviados: "))
+    mega = float(input("Ingrese el numero de megas consumidos: "))
+    minu = int(input("Ingrese el numero de minutos: "))
+    
+    costo = (0.80*sms) + (0.80*mega) + (0.80*minu)
+   
+print("El costo mensual es de: $",costo, "pesos")
 
 if __name__ == '__main__':
     main()
